3624143a934a262c3f406408c68124211ab5f653
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-19 16:19:50 -0500

    Set up firebase for this project and added the necessary calls and configurations to interact with it.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,19 @@ let firstTrainTime;
 let frequency;
 
 $(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyBF3_Tfv6mJKwrZhok3aCLTekOVmCQkRAw",
+        authDomain: "traintracker-96482.firebaseapp.com",
+        databaseURL: "https://traintracker-96482.firebaseio.com",
+        projectId: "traintracker-96482",
+        storageBucket: "",
+        messagingSenderId: "400661139958",
+        appId: "1:400661139958:web:6d6917253d25a5d029f7d7"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+
     $("#submitBtn").on("click", function(event){
         event.preventDefault();
         trainName = $("#trainNameInput").val().trim();
