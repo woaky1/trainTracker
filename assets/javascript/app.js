bb54961ad259eaef32f94a0d0538ba3243c4b190
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-19 16:12:57 -0500

    Got the submit button listener working and values can be console logged.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
 // We'll put some global variables here.
+let trainName;
+let destination;
+let firstTrainTime;
+let frequency;
 
 $(document).ready(function(){
-
+    $("#submitBtn").on("click", function(event){
+        event.preventDefault();
+        trainName = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstTrainTime = $("#firstTrainTimeInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
+        console.log(trainName + " " + destination + " " + firstTrainTime + " " + frequency);
+    });
 
 });
