40920954435a98471d0e2400e81e651583951cf3
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-20 16:35:28 -0500

    Did a bunch of documentation.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trainTracker
+
+This is a project I created as part of my studies at Northwestern's Coding Bootcamp. The page takes in train data and stores it in Firebase. We then can access that Firebase information to display information in a train time table. Since the data is stored in Firebase, multiple users can see and add information to the page at the same time. The page also features time until next train and next train to arrive data that updates automatically every minute.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,9 @@ let firstTrainTimeText = "";
 let frequencyText = "";
 var numberCheck;
 
+// We use document ready to make sure no javascript tries to fire before the page has fully loaded.
 $(document).ready(function(){
+    // Configuring Firebase
     var firebaseConfig = {
         apiKey: "AIzaSyBF3_Tfv6mJKwrZhok3aCLTekOVmCQkRAw",
         authDomain: "traintracker-96482.firebaseapp.com",
@@ -15,18 +17,18 @@ $(document).ready(function(){
         messagingSenderId: "400661139958",
         appId: "1:400661139958:web:6d6917253d25a5d029f7d7"
       };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
     var database = firebase.database();
     
+    // As part of our auto-update feature, we need a value that is unique to each train that we can refer to as we create new pieces of data. That way, we can refer to that number later when we update. That value is "trainIndex" and is obtained from Firebase. Here we grab that value to use in the next function.
     database.ref().on("value", function(snapshot) {
-        console.log(snapshot.val());
         var numberCheckValues = snapshot.val();
         numberCheck = numberCheckValues.trainIndex;
-        console.log("numberCheck: " + numberCheck);
     })
 
+    // This is an event listener looking for clicks on our form submit button. It takes the user data, saves it as variables and then sends them to Firebase for storage.
     $("#submitBtn").on("click", function(event){
         event.preventDefault();
         trainNameText = $("#trainNameInput").val().trim();
@@ -40,17 +42,19 @@ $(document).ready(function(){
             frequency: frequencyText,
             id: numberCheck
         };
-
+        
         database.ref("/trains").push(trainInfo);
+
+        // Here we update trainIndex, increasing it by one and sending the new value to Firebase. This will insure that the next train entered will get its own unique ID number.
         numberCheck++;
         database.ref().update({
             trainIndex: numberCheck
         });
     });
 
+    // Here we have a event listener that we use to display the data from Firebase in our table.
     database.ref("/trains").on("child_added", function(snapshot) {
         trainSnapshot = snapshot.val();
-        console.log(trainSnapshot);
         var newRow = $("<tr>").attr({"data-id":trainSnapshot.id,"data-first-train":trainSnapshot.firstTime,"data-frequency":trainSnapshot.frequency});
         var colName = $("<td scope='col'>").text(trainSnapshot.name);
         var colDestination = $("<td scope='col'>").text(trainSnapshot.destination);
@@ -60,6 +64,7 @@ $(document).ready(function(){
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
         var tRemainder = diffTime % trainSnapshot.frequency;
         var tMinutesTillTrain = trainSnapshot.frequency - tRemainder;
+        // Note in colMinTill and colNextArrival how we take the id number we set early and build it into the id of these td elements so we can update them later using a for loop.
         var colMinTill = $("<td scope='col' id='" + trainSnapshot.id + "min'>").text(tMinutesTillTrain);
         var nextTrain = moment().add(tMinutesTillTrain, "minutes");
         var colNextArrival = $("<td scope='col' id='" + trainSnapshot.id + "arrival'>").text(moment(nextTrain).format("hh:mm"));
@@ -67,13 +72,13 @@ $(document).ready(function(){
         $("#trainData").append(newRow);
     });
 
+    // This setInterval is what we use to update the minutes until the next train and next train arrival time every minute.
     var updateInterval = setInterval(updateTimes,60000);
 
     function updateTimes() {
         for (var i = 0; i < numberCheck; i++) {
             var refreshFirstTime = $("tr[data-id='" + i + "']").attr("data-first-train");
             var refreshFrequency = $("tr[data-id='" + i + "']").attr("data-frequency");
-            console.log(refreshFirstTime + refreshFrequency);
             var firstTimeConverted = moment(refreshFirstTime, "HH:mm").subtract(1, "years");
             var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
             var tRemainder = diffTime % refreshFrequency;
