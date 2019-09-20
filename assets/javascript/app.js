723c04ffd3fcde790bedbe564e7d7c22c9a33ac7
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-19 20:03:03 -0500

    Got displaying info from firebase working and the calculations for next arrival and minutes away working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,4 +36,22 @@ $(document).ready(function(){
         console.log(trainInfo);
         database.ref().push(trainInfo);
     });
+
+    database.ref().on("child_added", function(snapshot) {
+        trainSnapshot = snapshot.val();
+        console.log(trainSnapshot);
+        var newRow = $("<tr>");
+        var colName = $("<td scope='col'>").text(trainSnapshot.name);
+        var colDestination = $("<td scope='col'>").text(trainSnapshot.destination);
+        var colFrequency = $("<td scope='col'>").text(trainSnapshot.frequency);
+        var firstTimeConverted = moment(trainSnapshot.firstTime, "HH:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        var tRemainder = diffTime % trainSnapshot.frequency;
+        var tMinutesTillTrain = trainSnapshot.frequency - tRemainder;
+        var colMinTill = $("<td scope='col'>").text(tMinutesTillTrain);
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        var colNextArrival = $("<td scope='col'>").text(moment(nextTrain).format("hh:mm"));
+        newRow.append(colName).append(colDestination).append(colFrequency).append(colNextArrival).append(colMinTill);
+        $("#trainData").append(newRow);
+    });
 });
